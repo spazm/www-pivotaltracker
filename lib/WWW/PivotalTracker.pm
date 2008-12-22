766401f23cc36e582d1e01e44d7b74a4e5b5c42a
Jacob Helwig <jhelwig@cpan.org>
2008-12-22 00:58:47 -0800

    Version bump for CPAN release.

--- lib/WWW/PivotalTracker.pm
+++ lib/WWW/PivotalTracker.pm
@@ -26,11 +26,11 @@ WWW::PivotalTracker - Functional interface to Pivotal Tracker L<http://www.pivot
 
 =head1 VERSION
 
-Version 0.1.0_3
+Version 0.1.0_4
 
 =cut
 
-use version; our $VERSION = qv("0.1.0_3");
+use version; our $VERSION = qv("0.1.0_4");
 
 =head1 SYNOPSIS
 
