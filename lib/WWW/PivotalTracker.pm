spazm/www-pivotaltracker

commit 49e1d344766536377fa3b627a83fa57257a9141a

Minor cleanups.
* Make sure we're using Perl >= 5.6.
* Add an empty EXPORT list, and create an 'all' export tag.
* Only return the content from WWW::PivotalTracker::_post_request, if the
  request was successful.
* Remove AttrIndent option from XMLout in WWW::PivotalTracker::_make_xml,
  since we don't create XML with any attributes, anyway.

lib/WWW/PivotalTracker.pm
@@ -1,5 +1,7 @@
 package WWW::PivotalTracker;
 
+use 5.006;
+
 use warnings;
 use strict;
 
@@ -59,6 +61,14 @@ our @EXPORT_OK = qw/
     project_details
     show_story
 /;
+
+#This should never contain anything.  It's just here to make sure that :all
+#works as expected.
+our @EXPORT = ();
+
+our %EXPORT_TAGS = (
+    all => [ @EXPORT, @EXPORT_OK, ],
+);
 
 =head1 FUNCTIONS
 
@@ -347,12 +357,9 @@ sub _do_request($class, $token, $request_url, $request_method; $content)
     );
 
     my $response = $class->_post_request($request);
-    croak($response->status_line()) unless ($response->is_success());
-
-    print $response->content();
 
     return XMLin(
-        $response->content(),
+        $response,
         ForceArray => [qw/
             error
             iteration
@@ -372,15 +379,17 @@ sub _do_request($class, $token, $request_url, $request_method; $content)
 sub _post_request($class, $request)
 {
     my $ua = _UserAgent->new();
+    my $response = $ua->request($request);
 
-    return $ua->request($request);
+    croak($response->status_line()) unless ($response->is_success());
+
+    return $response;
 }
 
 sub _make_xml($class, HASH $data)
 {
     return XMLout(
         $data,
-        AttrIndent => 0,
         KeepRoot   => 1,
         NoAttr     => 1,
     );

MANIFEST.SKIP
@@ -1,6 +1,7 @@
 blib
 \.git
-\.gitignore
+\.gitignore$
+\.releaserc$
 ^Makefile$
 ^Makefile\.old$
 ^MANIFEST\.bak$

commit 7aca25a378e4552108d178f26cc35f399cf80253

Actually return content from _post_request.
This fixes breakage introduced in 49e1d344766536377fa3b627a83fa57257a9141a

Signed-off-by: Jacob Helwig <jhelwig@cpan.org>

lib/WWW/PivotalTracker.pm
@@ -91,7 +91,6 @@ sub project_details($token, $project_id) {
 
     my $response = __PACKAGE__->_do_request($token, "projects/$project_id", "GET");
 
-
     if (!defined $response || lc $response->{'success'} ne 'true') {
         return {
             success => 'false',
@@ -383,7 +382,7 @@ sub _post_request($class, $request)
 
     croak($response->status_line()) unless ($response->is_success());
 
-    return $response;
+    return $response->content();
 }
 
 sub _make_xml($class, HASH $data)